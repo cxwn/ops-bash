b83e5f720738ce0623afbdae146caf37cdc1e1a8
mrivandu <mrivandu@hotmail.com>
2019-04-30 02:24:59 +0800

    Update.

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Some scripts and tools used in daily operation and maintemance.
 - [源文件](https://github.com/mrivandu/ops/blob/master/sendmail.sh)
 
 - 当系统可用内存小于 1500MB 时，向指定的邮箱发送邮件。
+
+- 向 hotmail 邮箱发送邮件可能会失败。可以参考此脚本进行其他类似脚本的开发。邮件发送成功如下如：
--- sendmail.sh
+++ sendmail.sh
@@ -17,8 +17,8 @@ if [ -n "`sudo rpm -qa|grep mailx`" ];then
     yum -y install mailx
 fi
 
-if [ -z "`grep 'kxdrh' /etc/mail.rc`" ];then
-  echo "set from=kxdrh@163.com smtp=smtp.163.com smtp-auth-user=kxdrh smtp-auth-password=ylwan.0/7 smtp-auth=login">>/etc/mail.rc
+if [ -z "`grep 'gysl' /etc/mail.rc`" ];then
+  echo "set from=gysl@163.com smtp=smtp.163.com smtp-auth-user=gysl smtp-auth-password=r3P3GOd9ugZi smtp-auth=login">>/etc/mail.rc
 else
   echo "The username is exist. "
 fi
