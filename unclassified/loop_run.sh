7da4117b8a13de060e89938c46c21d9dfd2f1034
mrivandu <mrivandu@hotmail.com>
2019-06-05 12:33:13 +0800

    Update

--- unclassified/loop_run.sh
+++ unclassified/loop_run.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 cat>run.sh<<EOF
 #!/bin/bash
+counter=0
+round=200
 while true; do
-    counter=0
-    round=200
     dotnet TranscribeCore.dll -i sample -o \$(date +%F%H%M) -r 200 -e 500 -h 172.25.50.102 -p 34932 -n 2 -t 56 -l zh-CN>>14P5C56T-\${counter}.log
     if [ \$? -eq 0 -a \${counter} -le \${round}  ]; then
         ((counter++))
