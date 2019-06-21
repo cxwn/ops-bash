3c99d8afa752f907e94329db0b83c34084d94d95
mrivandu <mrivandu@hotmail.com>
2019-06-21 15:56:49 +0800

    Update

---
+++ CMCC/transcribe_statistisc_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+HOSTNAME="192.168.111.84"                                           #数据库信息
+PORT="20003"
+USERNAME="transcribe"
+DBNAME="transcribe"                                                       #数据库名称
+select  sum(TranscribeAllSeconds) as '音频时长' from transcribe_logs where PodType = 'AsyncService' and TranscribeDay >= '2019-05-10';;
+select  sum(TranscribeFrequency) as '转写成功量' from transcribe_logs where PodType = 'AsyncService' and TranscribeDay >= '2019-05-10';;
+select  ifnull(sum(TranscribeFrequency),0) as '转写错误量'  from transcribe_logs where PodType = 'AsyncServerError' and TranscribeDay >= '2019-05-10';;
+select  sum(TranscribeFrequency) as '转写量'  from transcribe_logs where (PodType = 'AsyncServerError' or PodType = 'AsyncService' ) and TranscribeDay >= '2019-05-10';;
+
+
+-- select(select  sum(TranscribeFrequency) as '转写成功量' from transcribe_logs where PodType = 'AsyncService' and TranscribeDay >= '2019-05-10';)
+-- /(select  sum(TranscribeFrequency) as '转写量'  from transcribe_logs where (PodType = 'AsyncServerError' or PodType = 'AsyncService' ) * 100 as '转写率' and TranscribeDay >= '2019-05-10';)
+
+select(select  sum(TranscribeFrequency) as '转写成功量' from transcribe_logs where PodType = 'AsyncService' and TranscribeDay >= '2019-05-10')
+/(select  sum(TranscribeFrequency) as '转写量'  from transcribe_logs where (PodType = 'AsyncServerError' or PodType = 'AsyncService' ) and TranscribeDay >= '2019-05-10') * 100 as '转写率';
+
+select  ifnull(sum(TranscribeFrequency),0) as '转写错误量'  from transcribe_logs where PodType = 'AsyncServerError' and TranscribeDay = '2019-05-10';
