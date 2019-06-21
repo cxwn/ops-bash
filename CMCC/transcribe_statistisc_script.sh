8118b664e10e755365b0e12701e6515ee5b21ab4
mrivandu <mrivandu@hotmail.com>
2019-06-21 17:00:34 +0800

    Update

--- CMCC/transcribe_statistisc_script.sh
+++ CMCC/transcribe_statistisc_script.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-HOSTNAME="192.168.111.84"                                           #数据库信息
-PORT="20003"
 USERNAME="transcribe"
-DBNAME="transcribe"                                                       #数据库名称
-select  sum(TranscribeAllSeconds) as '音频时长' from transcribe_logs where PodType = 'AsyncService' and TranscribeDay >= '2019-05-10';;
-select  sum(TranscribeFrequency) as '转写成功量' from transcribe_logs where PodType = 'AsyncService' and TranscribeDay >= '2019-05-10';;
-select  ifnull(sum(TranscribeFrequency),0) as '转写错误量'  from transcribe_logs where PodType = 'AsyncServerError' and TranscribeDay >= '2019-05-10';;
-select  sum(TranscribeFrequency) as '转写量'  from transcribe_logs where (PodType = 'AsyncServerError' or PodType = 'AsyncService' ) and TranscribeDay >= '2019-05-10';;
-
-
--- select(select  sum(TranscribeFrequency) as '转写成功量' from transcribe_logs where PodType = 'AsyncService' and TranscribeDay >= '2019-05-10';)
--- /(select  sum(TranscribeFrequency) as '转写量'  from transcribe_logs where (PodType = 'AsyncServerError' or PodType = 'AsyncService' ) * 100 as '转写率' and TranscribeDay >= '2019-05-10';)
-
-select(select  sum(TranscribeFrequency) as '转写成功量' from transcribe_logs where PodType = 'AsyncService' and TranscribeDay >= '2019-05-10')
-/(select  sum(TranscribeFrequency) as '转写量'  from transcribe_logs where (PodType = 'AsyncServerError' or PodType = 'AsyncService' ) and TranscribeDay >= '2019-05-10') * 100 as '转写率';
-
-select  ifnull(sum(TranscribeFrequency),0) as '转写错误量'  from transcribe_logs where PodType = 'AsyncServerError' and TranscribeDay = '2019-05-10';
+DBNAME="transcribe"
+DATE="2019-06-21" 
+mysql -u ${USERNAME} -h ${MYSQL_HOSTNAME} -P ${MYSQL_PORT} -p ${MYSQL_PASSWD}  -D ${DBNAME} <<EOF
+select current_date();
+use ${DBNAME};
+select  sum(TranscribeAllSeconds)/3600 as '音频总时长' from transcribe_logs where PodType = 'AsyncService' and TranscribeDay >= ${DATE};
+select  sum(TranscribeFrequency) as '转写成功量' from transcribe_logs where PodType = 'AsyncService' and TranscribeDay >= ${DATE};
+select  ifnull(sum(TranscribeFrequency),0) as '转写错误量'  from transcribe_logs where PodType = 'AsyncServerError' and TranscribeDay >= ${DATE};
+select  sum(TranscribeFrequency) as '转写量'  from transcribe_logs where (PodType = 'AsyncServerError' or PodType = 'AsyncService' ) and TranscribeDay >= ${DATE};
+select  ifnull(sum(TranscribeFrequency),0) as '转写错误量'  from transcribe_logs where PodType = 'AsyncServerError' and TranscribeDay = ${DATE};
+EOF
