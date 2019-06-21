33042ff1dac7768a3bed5a185877b6e611fe8f2c
mrivandu <mrivandu@hotmail.com>
2019-06-21 17:41:42 +0800

    Update

--- CMCC/transcribe_statistisc_script.sh
+++ CMCC/transcribe_statistisc_script.sh
@@ -5,9 +5,9 @@ DATE="2019-06-21"
 mysql -u ${USERNAME} -h ${MYSQL_HOSTNAME} -P ${MYSQL_PORT} -p ${MYSQL_PASSWD}  -D ${DBNAME} <<EOF
 select current_date();
 use ${DBNAME};
-select  sum(TranscribeAllSeconds)/3600 as '音频总时长' from transcribe_logs where PodType = 'AsyncService' and TranscribeDay >= ${DATE};
-select  sum(TranscribeFrequency) as '转写成功量' from transcribe_logs where PodType = 'AsyncService' and TranscribeDay >= ${DATE};
-select  ifnull(sum(TranscribeFrequency),0) as '转写错误量'  from transcribe_logs where PodType = 'AsyncServerError' and TranscribeDay >= ${DATE};
-select  sum(TranscribeFrequency) as '转写量'  from transcribe_logs where (PodType = 'AsyncServerError' or PodType = 'AsyncService' ) and TranscribeDay >= ${DATE};
-select  ifnull(sum(TranscribeFrequency),0) as '转写错误量'  from transcribe_logs where PodType = 'AsyncServerError' and TranscribeDay = ${DATE};
+select  sum(TranscribeAllSeconds)/3600 as 'Total audio length (hrs)' from transcribe_logs where PodType = 'AsyncService' and TranscribeDay >= ${DATE};
+select  sum(TranscribeFrequency) as 'Transcribing success' from transcribe_logs where PodType = 'AsyncService' and TranscribeDay >= ${DATE};
+select  ifnull(sum(TranscribeFrequency),0) as 'Transcribing erro'  from transcribe_logs where PodType = 'AsyncServerError' and TranscribeDay >= ${DATE};
+select  sum(TranscribeFrequency) as 'Transcribing quantity'  from transcribe_logs where (PodType = 'AsyncServerError' or PodType = 'AsyncService' ) and TranscribeDay >= ${DATE};
+select  ifnull(sum(TranscribeFrequency),0) as 'Transcribing error quantity'  from transcribe_logs where PodType = 'AsyncServerError' and TranscribeDay = ${DATE};
 EOF
